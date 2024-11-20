49f04e0e7a437af37295c67dfea75747d693f430
ioanmarincuta <ioanmarincuta@gmail.com>
2024-11-20 11:17:26 +0200

    Paginile aproape gata + adaugat codul de la colegi

--- pages/app/teacherpage/page.tsx
+++ pages/app/teacherpage/page.tsx
@@ -1,10 +1,232 @@
+"use client";
 import Link from "next/link";
+import * as React from "react";
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
 
-export default function MyAccount() {
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: "#d3d3d3",
+    color: "#000",
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
+function createData(
+  id: number,
+  name: string,
+  dataexamen: Date,
+  sala: string,
+  ora: number
+) {
+  const formattedDate = dataexamen.toLocaleDateString();
+  return { id, name, formattedDate, sala, ora };
+}
+
+export default function TeacherAccount() {
+  const [rows, setRows] = useState([
+    createData(1, "Fizica 1", new Date("2024-12-01"), "Sala 101", 12),
+    createData(
+      2,
+      "Matematici speciale",
+      new Date("2024-01-14"),
+      "Sala 202",
+      14
+    ),
+    createData(3, "PCLP1", new Date("2024-01-20"), "Sala 101", 12),
+    createData(4, "POO", new Date("2024-01-22"), "Sala 202", 14),
+    createData(5, "Analiza matematica", new Date("2024-01-26"), "Sala 101", 12),
+    createData(6, "Proiectare logica", new Date("2024-01-28"), "Sala 202", 14),
+    createData(
+      7,
+      "Proiectare interfete utlizator",
+      new Date("2024-01-30"),
+      "Sala 101",
+      12
+    ),
+    createData(8, "Fizica 2", new Date("2024-02-02"), "Sala 202", 14),
+  ]);
+
+  const [openDialog, setOpenDialog] = useState(false);
+  const [cancelMessage, setCancelMessage] = useState("");
+
+  const handleCancel = (id: number) => {
+    setRows((prevRows) => prevRows.filter((row) => row.id !== id));
+    setCancelMessage("Examenul a fost anulat!");
+    setOpenDialog(true);
+  };
   return (
     <>
-      <h1>Exaemenele mele ca si profesor</h1>
-      <Link href="/">Log out</Link>
+      <div
+        style={{
+          display: "flex",
+
+          justifyContent: "center",
+          alignItems: "center",
+          textAlign: "center",
+          marginBottom: "18px",
+        }}
+      >
+        <h1 style={{ marginTop: "40px" }}>Cereri de examen</h1>
+      </div>
+
+      <TableContainer
+        component={Paper}
+        style={{
+          marginLeft: "42px",
+          marginRight: "42px",
+          maxWidth: "calc(100% - 84px)",
+          margin: "0 auto",
+        }}
+      >
+        <Table
+          sx={{ minWidth: 700 }}
+          aria-label="customized table"
+          style={{ tableLayout: "fixed" }}
+        >
+          <TableHead>
+            <TableRow>
+              <StyledTableCell style={{ width: "20%", textAlign: "center" }}>
+                Materie
+              </StyledTableCell>
+              <StyledTableCell
+                style={{ width: "20%", textAlign: "center" }}
+                align="right"
+              >
+                Data examen
+              </StyledTableCell>
+              <StyledTableCell
+                style={{ width: "20%", textAlign: "center" }}
+                align="right"
+              >
+                Sala
+              </StyledTableCell>
+              <StyledTableCell
+                style={{ width: "20%", textAlign: "center" }}
+                align="right"
+              >
+                Ora
+              </StyledTableCell>
+              <StyledTableCell
+                style={{ width: "20%", textAlign: "center" }}
+                align="right"
+              >
+                Acțiune
+              </StyledTableCell>
+            </TableRow>
+          </TableHead>
+
+          <TableBody>
+            {rows.map((row) => (
+              <StyledTableRow key={row.id}>
+                <StyledTableCell
+                  component="th"
+                  scope="row"
+                  style={{ textAlign: "center" }}
+                >
+                  {row.name}
+                </StyledTableCell>
+                <StyledTableCell align="center">
+                  {row.formattedDate}
+                </StyledTableCell>
+                <StyledTableCell align="center" style={{ textAlign: "center" }}>
+                  {row.sala}
+                </StyledTableCell>
+                <StyledTableCell align="center" style={{ textAlign: "center" }}>
+                  {row.ora}
+                </StyledTableCell>
+                <StyledTableCell
+                  align="center"
+                  style={{
+                    textAlign: "center",
+                    display: "flex",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <Link href={`/examenprofesor`} passHref>
+                    <Button variant="outlined" size="small">
+                      Modifică
+                    </Button>
+                  </Link>
+                  <Link href={`/accepta/${row.id}`} passHref>
+                    <Button variant="outlined" size="small">
+                      Acceptă
+                    </Button>
+                  </Link>
+
+                  <Button
+                    variant="outlined"
+                    onClick={() => handleCancel(row.id)}
+                    size="small"
+                  >
+                    Anulează
+                  </Button>
+                </StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Dialog
+        open={openDialog}
+        onClose={() => setOpenDialog(false)}
+        sx={{
+          "& .MuiDialog-paper": {
+            backgroundColor: "#f0f8ff",
+            color: "#333",
+            borderRadius: "12px",
+            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+          },
+        }}
+      >
+        <DialogTitle
+          sx={{
+            backgroundColor: "#272F54",
+            color: "#ffffff",
+          }}
+        >
+          Mesaj
+        </DialogTitle>
+        <DialogContent
+          sx={{
+            backgroundColor: "#ffffff",
+            color: "#000000",
+          }}
+        >
+          <p>{cancelMessage}</p>
+        </DialogContent>
+        <DialogActions sx={{ color: "#272F54", backgroundColor: "#ffffff" }}>
+          <Button
+            onClick={() => setOpenDialog(false)}
+            sx={{ color: "#272F54", backgroundColor: "#ffffff" }}
+          >
+            OK
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
 }
